b35fae5e2ae9494d62671bc5ae0665c5004f88e2
ManuR8088 <vvce23cseaiml0132@vvce.ac.in>
2026-03-10 16:00:44 +0530

    Java file adding

---
+++ Myjava.java
@@ -0,0 +1,11 @@
+public class Myjava {
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = 1; i <= rows; i++) {       // Outer loop for rows
+            for (int j = 1; j <= i; j++) {     // Inner loop for stars
+                System.out.print("* ");
+            }
+            System.out.println();               // Move to next line
+        }
+    }
+}
